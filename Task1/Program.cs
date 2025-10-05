2d362d4e570903ce00139d755fd8dd9c0ca4a1d2
Kirya Pashkevich <pakir2005@gmail.com>
2025-10-06 01:29:16 +0300

    feat: add console calculator with input validation and basic operations

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,37 +2,107 @@
 {
     static void Main(string[] args)
     {
-        while (true)
+        int program = 0;
+        string inputString = "Enter first string";
+        string secondInputString = "Enter second string";
+        while (program == 0)
         {
+            double firstValue = GetValidDoubleInput(inputString);
+            Console.Clear();
+            double secondValue = GetValidDoubleInput(secondInputString);
+            Console.Clear();
+            char operationType = GetValidOperationInput();
+            Console.Clear();
+
+            switch (operationType)
+            {
+                case '+' :
+                    Console.WriteLine($"{firstValue} {operationType} {secondValue} = {firstValue + secondValue}",firstValue, operationType, secondValue);
+                    break;
+                case '-':
+                    Console.WriteLine($"{firstValue} {operationType} {secondValue} = {firstValue - secondValue}", firstValue, operationType, secondValue);
+                    break;
+                case '*':
+                    Console.WriteLine($"{firstValue} {operationType} {secondValue} = {firstValue * secondValue}", firstValue, operationType, secondValue);
+                    break;
+                case '/':
+                    Console.WriteLine($"{firstValue} {operationType} {secondValue} = {firstValue / secondValue}", firstValue, operationType, secondValue);
+                    break;
+
+            }
+            ContinueMessage();
+            Console.WriteLine("Do you want to exit?");
+            Console.WriteLine("If you want ot exit type \"Exit\"");
+            if (Console.ReadLine() == "Exit")  program = 1;
+            Console.Clear();
             
-          
 
 
         }          
     }
-    public static void VelcomeMessage()
+    public static char GetValidOperationInput()
     {
+        char[] validOperations = { '+', '-', '*', '/' };
+        bool isValid = false;
+        char operationType;
+        do
+        {
+            Console.WriteLine("Enter the type of operation:");
+            char.TryParse(Console.ReadLine(), out  operationType);
+            
+            if(!validOperations.Contains(operationType)){
+                Console.WriteLine("Error: invalid operation. Please enter one of +, -, *, /.");
+            
+                ContinueMessage();
+                continue;
+             
+            }
+            else
+            {
+                isValid = true;
+               
+            }
+           
+        }
+        while (isValid == false);
+
+        return operationType;
+    }
+    public static double GetValidDoubleInput(string input)
+    {
+        double number;
+        bool isValid = false;
+        do
+        {
+            Console.WriteLine(input);
+            if(double.TryParse(Console.ReadLine(),out number))
+            {
+                if (input.Contains("second"))
+                {
+                    if (number == 0)
+                    {
+                        Console.WriteLine("You trying to divide by zero ");
+                        ContinueMessage();
+                        continue;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Eror: incorrect input");
+                ContinueMessage();
+                continue;
+            }
+            isValid = true;
+        }
+        while (isValid == false);
+        return number;
+    } 
+    
+    public static void ContinueMessage()
+    {
+        Console.WriteLine("Press any buttun to continue");
         Console.ReadKey();
         Console.Clear();
-        Console.WriteLine("Welcome to calculator");
-        Console.WriteLine("Enter number of operation what do you want to do now:");
-        Console.WriteLine("Type 1 to start calculating");
-        Console.WriteLine("Type 2 to exit from program");
-    }
-    public static void EnterNumbersAndOperation()
-    {
-        Console.Write("Enter the fist number: ");
-        double first_number = double.Parse(Console.ReadLine());
-
-        Console.Write("\n Enter the operation(*, /, +, -)");
-        char operation_type = Char.Parse(Console.ReadLine());
-
-        Console.Write("\n Enter the second number: ");
-        double second_number = double.Parse(Console.ReadLine());
-    }
-    public static void ErrorMessage(string message)
-    {
-        Console.WriteLine("You are made this eror:");
-        Console.WriteLine(message);
     }
 }
